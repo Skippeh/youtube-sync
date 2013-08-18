e77f5edb2664e94c4a0da598c8ca1b1e2f1a7896
Jonathan <jonathanjl99@gmail.com>
2013-08-18 16:42:40 +0200

    Videos now flash in the playlist when added.

--- Website/JS/Playlist.js
+++ Website/JS/Playlist.js
@@ -9,12 +9,16 @@ Playlist.Add = function(title, author, length, videoId, uniqueId, channelImage)
 		title = title.substring(0, 42) + "...";
 
 	playlistEntries.append(
-		"<div id=\"playlistEntry_" + uniqueId + "\" class=\"playlistEntry\">" +
+		"<div id=\"playlistEntry_" + uniqueId + "\" class=\"playlistEntry\" style=\"background-color: rgba(51, 119, 219, 0.5);\">" +
 			"<a target=\"_blank\" href=\"http://www.youtube.com/watch?feature=player_embedded&v=" + videoId + "\"><img class=\"channelImage\" src=\"" + channelImage + "\" width=\"21\" height=\"21\" title=\"Goto video page\"/></a>" +
 			"<span class=\"videoTitle\" title=\"" + fullTitle + "\">" + title + "</span><br/>" +
 			"<span class=\"videoDetails\">Uploaded by " + author + " " + length + "</span>" +
 		"</div>"
 	);
+	
+	var elm = $("#playlistEntry_" + uniqueId);
+	//elm.css("background-color", "#FF0");
+	elm.animate({ "background-color": "rgba(0,0,0,0)" }, 350);
 	
 	$(".scrollbar").TrackpadScrollEmulator("recalculate");
 };
